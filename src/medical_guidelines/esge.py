9aba1188484ecba0278ffc2809d52606ef897106
uvdivergence <uvdivergence@proton.me>
2026-03-07 21:01:24 +0100

    chore: esge index downloader

---
+++ src/medical_guidelines/esge.py
@@ -0,0 +1,113 @@
+import re
+
+from collections.abc import Callable, Iterable
+from typing import Any
+
+import aiohttp
+
+from bs4 import BeautifulSoup
+from pydantic import BaseModel
+from yarl import URL
+
+
+_BASE_URL = URL("https://www.esge.com/")
+_PAGE_API_URL = _BASE_URL / "api" / "live" / "pages"
+_DOI_PATTERN = re.compile(r"10\.\d{4,9}\/[-._;()/a-zA-Z0-9]+")
+_MALFORMED_DOIS = {
+    "DOI https://doi.org/0.1055/a-2292-2494": "10.1055/a-2292-2494"
+}
+
+
+class Entry(BaseModel):
+    name: str
+    doi: str
+    url: URL
+
+
+type Index = list[Entry]
+type _Node = dict[str, Any]
+
+
+def _find_nodes(
+    root: _Node,
+    predicate: Callable[[_Node], bool]
+) -> Iterable[_Node]:
+    candidates = [root]
+
+    while candidates:
+        candidate = candidates.pop()
+
+        if predicate(candidate):
+            yield candidate
+
+        candidates.extend(candidate.get("children", []))
+
+
+def _find_nodes_by_title(root: _Node, title: str) -> Iterable[_Node]:
+    return _find_nodes(root, lambda x: x.get("title") == title)
+
+
+def _extract_doi(text: str) -> str:
+    if text in _MALFORMED_DOIS:
+        return _MALFORMED_DOIS[text]
+
+    match = _DOI_PATTERN.search(text)
+
+    if not match:
+        raise ValueError(f"Couldn't extract DOI from '{text}'")
+
+    return match.group()
+
+
+async def download_index() -> Index:
+    index = []
+
+    async with aiohttp.ClientSession() as session:
+        detail_urls = []
+
+        async with session.get(_PAGE_API_URL / "guidelines") as response:
+            data = await response.json()
+
+        for node in _find_nodes_by_title(data["content"], "copy list"):
+            html = node["properties"]["text"]
+            soup = BeautifulSoup(html, "html.parser")
+
+            for anchor in soup.find_all("a"):
+                href = anchor["href"]
+
+                if "://" in href:
+                    detail_url = _PAGE_API_URL / URL(href).path[1:]
+                else:
+                    detail_url = _PAGE_API_URL / href[1:]
+                
+                detail_urls.append(detail_url)
+
+        for detail_url in detail_urls:
+            async with session.get(detail_url) as response:
+                data = await response.json()
+
+            content = data["content"]
+            button_node = next(
+                _find_nodes_by_title(content, "View full guideline")
+            )
+
+            doi_node = next(_find_nodes_by_title(content, "Link + Copyright"))
+            html = doi_node["properties"]["text"]
+            soup = BeautifulSoup(html, "html.parser")
+
+            entry_name = content["title"]
+            entry_url = \
+                _BASE_URL / button_node["properties"]["targetName"][1:]
+
+            entry_doi = \
+                _extract_doi(list(soup.find("h6").children)[0].get_text())
+
+            entry = Entry(
+                name=entry_name,
+                url=entry_url,
+                doi=entry_doi
+            )
+
+            index.append(entry)
+
+    return index
